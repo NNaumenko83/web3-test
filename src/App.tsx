d40ec825ea3c74bd2fa36e9deda14c47ba78470a
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2025-09-02 17:57:26 +0300

    feat: add web3 integration to fetch Ethereum account balance
    - Updated package.json to include web3 dependency
    - Implemented functionality in App.tsx to connect to Infura and retrieve the balance of a specified Ethereum account
    - Displayed account address and balance in the UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
 
 
+import Web3, { type Numbers } from 'web3';
 import './App.css'
+import { useEffect, useState } from 'react';
+
+const INFURA_API_KEY = import.meta.env.VITE_APP_INFURA_API_KEY
+const PROJECT_ID = import.meta.env.VITE_APP_PROJECT_ID
+
+const INFURA_URL = `https://mainnet.infura.io/v3/${INFURA_API_KEY}`
 
 function App() {
+  const [balance, setBalance] = useState<string | null>(null);
 
+  const web3 = new Web3(INFURA_URL);
+  const account = "0x90e63c3d53E0Ea496845b7a03ec7548B70014A91"
+
+  useEffect(() => {
+    const getAccountBalance = async () => {
+      try {
+        const wei = await web3.eth.getBalance(account);
+        const balance = web3.utils.fromWei(wei, 'ether');
+        setBalance(balance);
+      } catch (err) {
+        console.error('Error fetching balance:', err);
+      }
+    }
+
+    getAccountBalance();
+  }, [account, web3.eth]);
 
   return (
     <>
       <h1> Web3 test</h1>
+      <p>Account: {account}</p>
+      <p>Balance: {balance} ETH</p>
     </>
   )
 }
